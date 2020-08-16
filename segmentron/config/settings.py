97bebb3880fb69760cc951436621d88b45b96647
xieenze <Jonnhy_ez@163.com>
2020-08-16 22:42:19 +0800

    worker=8

--- segmentron/config/settings.py
+++ segmentron/config/settings.py
@@ -22,7 +22,7 @@ cfg.DATASET.STD = [0.5, 0.5, 0.5]
 # dataset ignore index
 cfg.DATASET.IGNORE_INDEX = -1
 # workers
-cfg.DATASET.WORKERS = 4
+cfg.DATASET.WORKERS = 8
 # val dataset mode
 cfg.DATASET.MODE = 'testval'
 ########################### data augment ######################################
